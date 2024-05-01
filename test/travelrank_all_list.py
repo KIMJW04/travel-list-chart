f2a2b7d2be798759ce3fe8831969daa4c29e8567
JinWoo Kim <kjw040416@gmail.com>
2024-05-01 17:39:05 +0900

    2024.05.01

--- test/travelrank_all_list.py
+++ test/travelrank_all_list.py
@@ -9,6 +9,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import time
 import json
+import os
 
 # 현재 날짜 가져오기
 current_date = datetime.now().strftime("%Y-%m-%d")
@@ -284,51 +285,54 @@ korean_administrative_units_list = {
 options = ChromeOptions()
 service = ChromeService(executable_path=ChromeDriverManager().install())
 
+
 for kaul in korean_administrative_units_list:
     print(kaul)
+    def create_travelrank_list(data, parent_folder_name="."):
+        folder_path = os.path.join(parent_folder_name, kaul)
+        os.makedirs(folder_path, exist_ok=True)
+    create_travelrank_list(kaul)
+    # 각 지역에 대한 정보를 가져와서 파일로 저장
     for region in korean_administrative_units_list[kaul]:
-        region_en = ragion
+        # 영어 지역명 가져오기
+        region_en = korean_administrative_units_list[kaul][region]
+        print(region)
         print(region, region_en)
 
+        # 지역명을 URL에 포함하여 URL 생성
+        url = f"https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query={region}+여행"
+        filename = f"chart_travel_{region_en}-{current_date}.json"
 
-# 각 지역에 대한 정보를 가져와서 파일로 저장
-# for region in region_dict:
-#     # 영어 지역명 가져오기
-#     region_en = region_dict[region]
-#     print(region)
+        # 웹드라이버 초기화
+        browser = webdriver.Chrome(service=service, options=options)
+        browser.get(url)
 
-#     # 지역명을 URL에 포함하여 URL 생성
-#     url = f"https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query={region}+여행"
-#     filename = f"chart_travel_{region_en}-{current_date}.json"
+        # 페이지가 완전히 로드될 때까지 대기
+        WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, "main_pack")))
 
-#     # 웹드라이버 초기화
-#     browser = webdriver.Chrome(service=service, options=options)
-#     browser.get(url)
+        # 업데이트된 페이지 소스를 변수에 저장
+        html_source_updated = browser.page_source
+        soup = BeautifulSoup(html_source_updated, 'html.parser')
 
-#     # 페이지가 완전히 로드될 때까지 대기
-#     WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, "main_pack")))
+        # 데이터 추출
+        travel_data = []
+        travels_list = soup.select("#nxTsDo > div > div > div.do_content > div > div:nth-child(5) > div.ScrollBox-mso6a.Poi_filteredPoi-vqFVt.Poi_filteredPoi-HVLwu > div.information-iiK8v > ul > li.item-wJaHc")
+        for travel in travels_list:
+            ranking = travel.find('div', class_='rank-kEDoI').text.strip()
+            title = travel.find('span', class_='name-K_anJ').text.strip()
+            img_tag = travel.find('img', class_='img-q5u9H')['src']
+            link_tag = travel.find('a', class_='anchor-X0MS6')['href']
+            travel_data.append({
+                'ranking': ranking,
+                'title': title,
+                'image_url': img_tag,
+                'link': link_tag
+            })
+            
+        filename = os.path.join(kaul, f"chart_travel_{region_en}-{current_date}.json")
 
-#     # 업데이트된 페이지 소스를 변수에 저장
-#     html_source_updated = browser.page_source
-#     soup = BeautifulSoup(html_source_updated, 'html.parser')
-
-#     # 데이터 추출
-#     travel_data = []
-#     travels_list = soup.select("#nxTsDo > div > div > div.do_content > div > div:nth-child(5) > div.ScrollBox-mso6a.Poi_filteredPoi-vqFVt.Poi_filteredPoi-HVLwu > div.information-iiK8v > ul > li.item-wJaHc")
-#     for travel in travels_list:
-#         ranking = travel.find('div', class_='rank-kEDoI').text.strip()
-#         title = travel.find('span', class_='name-K_anJ').text.strip()
-#         img_tag = travel.find('img', class_='img-q5u9H')['src']
-#         link_tag = travel.find('a', class_='anchor-X0MS6')['href']
-#         travel_data.append({
-#             'ranking': ranking,
-#             'title': title,
-#             'image_url': img_tag,
-#             'link': link_tag
-#         })
-        
-#     # 데이터를 JSON 파일로 저장
-#     with open(filename, 'w', encoding='utf-8') as f:
-#         json.dump(travel_data, f, ensure_ascii=False, indent=4)
-#     # 브라우저 종료
-#     browser.quit()
+        # 데이터를 JSON 파일로 저장
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(travel_data, f, ensure_ascii=False, indent=4)
+        # 브라우저 종료
+        browser.quit()
